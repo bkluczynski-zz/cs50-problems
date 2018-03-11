b25cb5ed8970f622b89ca800e89128e2b7099182
Bart <bart@paybase.io>
2018-03-11 22:31:02 +0000

    mario pyramide solution in c

---
+++ mario.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    //get height
+    int height = get_int("Specify the height of your pyramide: \n");
+    while (height > 23 || height < 0)
+    {
+        height = get_int("Specify the height of your pyramide: \n");
+    }
+    //first loop to iterate over all rows
+    for (int i = 0; i < height; i++)
+    {
+        //second loop nested inside a row to calculate white spaces
+        for (int j = i; j < height - 1; j++)
+        {
+            //print white spaces
+            printf(" ");
+        }
+        //third loop nested inside a row to calculate number of hashes
+        for (int k = i + 2; k > 0; k--)
+        {
+            //print hashes
+            printf("#");
+        }
+        //print linebreak
+        printf("\n");
+    }
+}
